1a17b72ca4728618bf7838c94987015ee0b67ef7
Prachi Jindal <prachijindal402@gmail.com>
2024-11-19 15:03:10 +0530

    adding basic models

---
+++ models/tasks.models.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+new taskSchema = mongoose.Schema(
+    {
+        Title :{
+            type : String,
+            required : true, 
+            unique : true
+        },
+        Status :{
+            type : String,
+            default : "Pending"
+        },
+        Description :{
+            type : Text,
+        },
+        Priority: {
+            type : String,
+            required : true,
+            default : "Low"
+        },
+        Deadline : {
+            type : Date
+        },
+        Date_Created :{
+            type : Date
+        }
+    }
+)
+
+export const Task = mongoose.model("Task",taskSchema)  
