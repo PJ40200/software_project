63004c214800daa84d42e5fdafd77d4cbfcf9d76
Prachi Jindal <prachijindal402@gmail.com>
2024-11-26 16:05:36 +0530

    reorganise

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path : './env'})
+
 import mongoose from "mongoose";
 import {DB_NAME} from "./constants";
 import express from "express";
@@ -13,7 +15,7 @@ const app= express()
         })
 
         app.listen(process.enc.PORT, () => {
-            console.log("App is listening on port ${process.env.PORT}");
+            console.log("App is listening on PORT = ${process.env.PORT}");
         })
     }
     catch(error){
